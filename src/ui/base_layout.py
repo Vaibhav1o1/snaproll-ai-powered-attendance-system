f92232c6226ac6e6fc084dd3f574eff6033a2321
Vaibhav Gupta <techvaibhav27@gmail.com>
2026-06-20 18:44:19 +0530

    Teacher Manage Subjects Dashboard

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -91,7 +91,7 @@ def style_base_layout():
 
                 button { 
                     border-radius: 1.5rem !important;
-                    background: #5865F2 !important;
+                    background-color: #5865F2 !important;
                     color: white !important;
                     padding: 10px 20px !important;
                     border: none !important;
@@ -102,7 +102,7 @@ def style_base_layout():
 
                 button[kind = "secondary"] {
                     border-radius: 1.5rem !important;
-                    background: #EB459E !important;
+                    background-color: #EB459E !important;
                     color: white !important;
                     padding: 10px 20px !important;
                     border: none !important;
@@ -113,7 +113,7 @@ def style_base_layout():
 
                  button[kind = "tertiary"] {
                     border-radius: 1.5rem !important;
-                    background: black !important;
+                    background-color: black !important;
                     color: white !important;
                     padding: 10px 20px !important;
                     border: none !important;
