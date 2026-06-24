25480b44331070ba95f16a2c9d4c1dab784be62f
Vaibhav Gupta <techvaibhav27@gmail.com>
2026-06-25 03:36:36 +0530

    Add Photos Flow

---
+++ src/components/dialog_add_photos.py
@@ -0,0 +1,57 @@
+import streamlit as st
+from PIL import Image
+
+
+@st.dialog("Capture or Upload Photos")
+def add_photos_dialog():
+    st.write('Add Classroom Photos to Scan for Attendance')
+
+    if 'photo_tab' not in st.session_state:
+        st.session_state.photo_tab = 'camera'
+
+    t1, t2 = st.columns(2) # Two Tabs
+
+    with t1:
+        type_camera = "primary" if st.session_state.photo_tab == 'camera' else "tertiary"
+        if st.button('Capture using Camera', type=type_camera, width="stretch"):
+            st.session_state.photo_tab = 'camera'
+
+    with t2:
+        type_upload = "primary" if st.session_state.photo_tab == 'camera' else "tertiary"
+        if st.button('Upload Photos', type=type_upload, width="stretch"):
+            st.session_state.photo_tab = 'upload'
+
+    
+    if st.session_state.photo_tab == 'camera':
+        cam_photo = st.camera_input('Take Snapshot', key='dialog_camera')
+
+        if cam_photo:
+            st.session_state.attendance_images.append(Image.open(cam_photo))
+            st.toast('Photos Captured Successfully!')
+            st.rerun()
+
+
+    if st.session_state.photo_tab == 'upload':
+        uploaded_files = st.file_uploader('Upload Images', key='dialog_upload', type=['jpg', 'png', 'jpeg'], accept_multiple_files=True)
+
+        if uploaded_files:
+            for f in uploaded_files:
+                st.session_state.attendance_images.append(Image.open(f))
+
+            st.toast('Photos Uploaded Successfully!')
+            st.rerun()
+
+    
+    st.divider()
+
+    if st.button('Done', type="primary", width="stretch"):
+        st.rerun()
+
+
+
+
+
+
+
+    
+
--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -10,6 +10,7 @@ from src.database.db import check_teacher_exists, create_teacher, teacher_login,
 
 from src.components.dialog_create_subject import create_subject_dialog
 from src.components.dialog_share_subject import share_subject_dialog
+from src.components.dialog_add_photos import add_photos_dialog
 
 from src.components.subject_card import subject_card
 
@@ -93,7 +94,38 @@ def teacher_dashboard():
 
 
 def teacher_tab_take_attendance():
+    teacher_id = st.session_state.teacher_data['teacher_id']
     st.header("Take AI Attendance")
+
+
+    if 'attendance_images' not in st.session_state:
+        st.session_state.attendance_images = []
+
+    subjects = get_teacher_subjects(teacher_id)
+
+    if not subjects:
+        st.warning("You have not created any subject yet! Please create one to begin!")
+        return
+    
+    subject_options = {f"{s['name']} - {s['subject_code']}": s['subject_id'] for s in subjects}
+
+    col1, col2 = st.columns([3, 1]) # First col is 3 times bigger than second col.
+
+    with col1:
+        selected_subject_label = st.selectbox('Select Subject', options=list(subject_options.keys()))
+
+    with col2:
+        if st.button('Add Photos', type="primary", icon=":material/photo_prints:", width="stretch"):
+            add_photos_dialog()
+
+    
+    selected_subject_id = subject_options[selected_subject_label]
+
+    st.divider()
+
+
+
+
 
 
 def teacher_tab_manage_subjects():
